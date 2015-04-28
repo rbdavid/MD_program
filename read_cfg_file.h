51e197d459c5322949c105275a545acc2c449542
rbdavid <russell.davidson@colostate.edu>
2015-04-28 01:37:21 -0600

    Moved subroutines to separate files; introduced initial positions and force/energy calcs

---
+++ read_cfg_file.h
@@ -0,0 +1,5 @@
+/* file read_cfg_file.h */
+// header file for the read_cfg_file subroutine
+
+void read_cfg_file(char *, char *, double *, int *, int *, int *, int *, int *, double *, double *, double *);
+
